ba395162e7a449f1feccbe6d2f60c6f68592b76c
Tobias Castañeda <tobias.2000david@gmail.com>
2023-05-06 12:16:48 -0300

    AGrego inicio sesion en usuario

--- src/main/java/Usuario/Usuario.java
+++ src/main/java/Usuario/Usuario.java
@@ -1,12 +1,18 @@
 package Usuario;
 
+import java.util.Objects;
+
 public class Usuario {
     String nombre;
     String contrasenia;
+    int intentos;
+    boolean sesionAbierta;
 
     public Usuario(String nombre, String contrasenia) {
         this.nombre = nombre;
         this.contrasenia = contrasenia;
+        this.intentos = 3;
+        this.sesionAbierta = false;
     }
 
     public String getNombre() {
@@ -15,4 +21,21 @@ public class Usuario {
     public String getContrasenia(){
         return this.contrasenia;
     }
+
+    public boolean isSesionAbierta() {
+        return sesionAbierta;
+    }
+
+    public void iniciarSesion(String username, String contrasenia){
+        if(this.intentos==0){
+            throw new RuntimeException("Se ha superado limite de intentos e inicio de sesion");
+        }
+        if(this.nombre.equals( username) && this.contrasenia.equals(contrasenia)){
+            sesionAbierta = true;
+        }
+        else {
+            this.intentos--;
+        }
+    }
+
 }
