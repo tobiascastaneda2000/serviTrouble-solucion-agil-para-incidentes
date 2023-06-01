5e4209c55f031471c9771dadb96d954463a33b0a
carrilloFN <fcarrillo@frba.utn.edu.ar>
2023-06-01 19:57:00 -0300

    Agregado de dependencias Retrofit con Geo-Ref API

--- src/main/java/ar/edu/utn/frba/dds/LOCALIZACION.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.utn.frba.dds;
-
-public class LOCALIZACION {
-    //TODO usar api
-}
---
+++ src/main/java/ar/edu/utn/frba/dds/Localizacion.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frba.dds;
+
+/*
+ * Adaptador de API:
+ *  Debe permitir obtener: Municipios - Departamentos - Provincias
+ */
+public class Localizacion {
+
+  private final String url = "https://apis.datos.gob.ar/georef/api/";
+
+  public Localizacion(){}
+
+  public String obtenerProvincias(){
+
+  }
+
+  public realizarRequest(String params){
+
+  }
+
+}
