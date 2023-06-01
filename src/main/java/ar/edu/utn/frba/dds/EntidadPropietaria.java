71a9c6df1ac3f40e0455f9565bf7d43ffa0acd61
tobiasiglesias <tobias.nehuenn@gmail.com>
2023-05-31 22:47:47 -0300

    cambios a entidadPropietario

--- src/main/java/ar/edu/utn/frba/dds/EntidadPropietaria.java
+++ src/main/java/ar/edu/utn/frba/dds/EntidadPropietaria.java
@@ -2,10 +2,19 @@ package ar.edu.utn.frba.dds;
 
 import ar.edu.utn.frba.dds.usuario.Usuario;
 import ar.edu.utn.frba.dds.Entidad;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class EntidadPropietaria {
     Usuario usuarioEncargado;
     List<Entidad> entidades;
-    
+
+    public EntidadPropietaria(Usuario usuarioEncargado){
+        this.entidades = new ArrayList<>();
+        this.usuarioEncargado = usuarioEncargado;
+    }
+    public void recopilarProblematicas() {
+        
+    }
 }
