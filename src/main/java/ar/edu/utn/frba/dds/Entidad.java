8aa9ff489a226ed41f2bca367c3dfc28d8c0dd32
tobiascastaneda2000 <tobias.2000david@gmail.com>
2023-07-03 09:02:24 -0300

    Implemento entidades  y establecimientos

--- src/main/java/ar/edu/utn/frba/dds/Entidad.java
+++ src/main/java/ar/edu/utn/frba/dds/Entidad.java
@@ -1,9 +1,14 @@
 package ar.edu.utn.frba.dds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Entidad {
     private int id;
     private String razonSocial;
     private String email;
+
+    public List<Establecimiento> establecimientos = new ArrayList<>();
 
     public Entidad(int id, String razonSocial, String email) {
         this.id = id;
--- src/main/java/ar/edu/utn/frba/dds/Establecimiento.java
+++ src/main/java/ar/edu/utn/frba/dds/Establecimiento.java
@@ -1,4 +1,8 @@
 package ar.edu.utn.frba.dds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Establecimiento {
+  List<TipoServicio> servicio = new ArrayList<>();
 }
