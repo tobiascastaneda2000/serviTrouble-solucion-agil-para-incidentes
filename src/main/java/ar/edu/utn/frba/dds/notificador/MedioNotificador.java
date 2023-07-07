361bb8e070ab49af73321e36abb269e28d385300
tobiasiglesias <tobias.nehuenn@gmail.com>
2023-07-06 22:50:20 -0300

    Rename del metodo notificar

--- src/main/java/ar/edu/utn/frba/dds/notificador/MedioNotificador.java
+++ src/main/java/ar/edu/utn/frba/dds/notificador/MedioNotificador.java
@@ -1,11 +1,9 @@
 package ar.edu.utn.frba.dds.notificador;
 
 
-import ar.edu.utn.frba.dds.Servicio;
 import ar.edu.utn.frba.dds.comunidad_e_incidentes.Incidente;
-import ar.edu.utn.frba.dds.Usuario;
 
 public interface MedioNotificador {
 
-  void notificar(String mensaje, Usuario usuario, Servicio servicio);
+  void notificar(Incidente incidente);
 }
