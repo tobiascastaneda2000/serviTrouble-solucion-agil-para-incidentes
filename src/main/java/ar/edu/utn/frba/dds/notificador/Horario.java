e49c98c681d22018d981d09c812f9391e4c07e19
carrilloFN <fcarrillo@frba.utn.edu.ar>
2023-09-15 10:22:33 -0300

    Entity de Horario

--- src/main/java/ar/edu/utn/frba/dds/notificador/Horario.java
+++ src/main/java/ar/edu/utn/frba/dds/notificador/Horario.java
@@ -1,8 +1,26 @@
 package ar.edu.utn.frba.dds.notificador;
 
+import ar.edu.utn.frba.dds.Usuario;
+import ar.edu.utn.frba.dds.comunidad_e_incidentes.Incidente;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+@Entity
 public class Horario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
   private int hora;
   private int minuto;
+
+  @ManyToOne
+  @JoinColumn(name = "usuario_id")
+  public Usuario usuario;
 
   public Horario(int hora, int minuto) {
     this.hora = hora;
