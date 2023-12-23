cbdc53a719c4732913a4dcb6cbc8251f599a3b56
tobiasiglesias <tobias.nehuenn@gmail.com>
2023-12-23 20:39:13 -0300

    Agrego withTransaction

--- src/main/java/ar/edu/utn/frba/dds/NotificadorProgramado.java
+++ src/main/java/ar/edu/utn/frba/dds/NotificadorProgramado.java
@@ -9,23 +9,27 @@ import ar.edu.utn.frba.dds.rankings.CantidadReportesSemanal;
 import ar.edu.utn.frba.dds.rankings.CriterioRanking;
 import ar.edu.utn.frba.dds.rankings.PromedioCierresSemanal;
 import ar.edu.utn.frba.dds.repositorios.RepoEntidades;
+import io.github.flbulgarelli.jpa.extras.test.SimplePersistenceTest;
 import ar.edu.utn.frba.dds.repositorios.RepoUsuarios;
 
 import java.util.*;
 import java.util.Set;
 import java.time.LocalDateTime;
 
-public class NotificadorProgramado {
+public class NotificadorProgramado implements SimplePersistenceTest{
 
   public static void main(String[] args) {
     new NotificadorProgramado().run();
   }
 
   public void run() {
-    Set<Usuario> usuarios = RepoUsuarios.getInstance().getUsuariosComoSet();
-    LocalDateTime ahora = LocalDateTime.now();
-    int hora = ahora.getHour();
-    int minuto = ahora.getMinute();
-    usuarios.forEach(u -> u.verificarNotificaciones(new Horario(hora,minuto)));
+    withTransaction(() -> {
+      Set<Usuario> usuarios = RepoUsuarios.getInstance().getUsuariosComoSet();
+      LocalDateTime ahora = LocalDateTime.now();
+      int hora = ahora.getHour();
+      int minuto = ahora.getMinute();
+      usuarios.forEach(u -> u.verificarNotificaciones(new Horario(hora,minuto)));
+    });
+
   }
 }
